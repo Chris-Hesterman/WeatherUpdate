df26f6f7ac8d7206463c7db0d51a3e91e0697e4a
Chris Hesterman <spoiler14@comcast.net>
2019-09-18 17:10:22 -0700

    still working out mobile timing

--- js/index.js
+++ js/index.js
@@ -38,9 +38,9 @@ $(document).ready(function() {
     $(".icon").on('click', function() {
       $(this).fadeOut(300);
       if($(window).width() < 851) {
-        setTimeout(function() {
-          $("html").scrollTop(window.innerHeight);
-        },500);
+        
+        $("html").scrollTop(window.innerHeight);
+       
         $("input").delay(500).animate({
           width: "80vw",
           padding: "5px 0 5px 1rem"
