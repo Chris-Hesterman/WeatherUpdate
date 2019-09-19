6d0977e15ec6e5ad20a3fcf3f02ee1b88e42c7b7
Chris Hesterman <spoiler14@comcast.net>
2019-09-18 17:24:22 -0700

    trying to target correct element for mobile input issue

--- js/index.js
+++ js/index.js
@@ -39,8 +39,8 @@ $(document).ready(function() {
       $(this).fadeOut(300);
       if($(window).width() < 851) {
         
-        $("html").scrollTop(window.innerHeight);
-       
+        $("html body .backImg").scrollTop(window.innerHeight);
+       //am I targeting right element?
         $("input").delay(500).animate({
           width: "80vw",
           padding: "5px 0 5px 1rem"
